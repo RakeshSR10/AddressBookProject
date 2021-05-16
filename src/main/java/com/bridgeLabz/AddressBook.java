3a538aa8ecfb5c030a82b7ff34f2c7b1c6aa9f33
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-16 20:01:48 +0530

    Added: Ability to edit existing person contact details

--- src/main/java/com/bridgeLabz/AddressBook.java
+++ src/main/java/com/bridgeLabz/AddressBook.java
@@ -35,10 +35,41 @@ public class AddressBook {
 
         System.out.println(first_Name+", "+last_Name+", "+city+", "+state+", "+address+", "+phoneNumber+", "+zip+", "+email);
     }
+
+    //UC3 - Edit Contact
+    private void editContact() {
+        System.out.println("Enter name of the person to edit details:");
+        String editName = scanner.next();
+        if(editName.equalsIgnoreCase(first_Name)){
+            System.out.println("Edit First Name:");
+            first_Name = scanner.next();
+            System.out.println("Edit Last Name:");
+            last_Name = scanner.next();
+            System.out.println("Edit City Name:");
+            city = scanner.next();
+            System.out.println("Edit State Name:");
+            state = scanner.next();
+            System.out.println("Edit Address:");
+            address = scanner.next();
+            System.out.println("Edit Phone Number:");
+            phoneNumber = scanner.next();
+            System.out.println("Edit Zip:");
+            zip = scanner.next();
+            System.out.println("Edit Email:");
+            email = scanner.next();
+
+            System.out.println(first_Name+", "+last_Name+", "+city+", "+state+", "+address+", "+phoneNumber+", "+zip+", "+email);
+        }else {
+            System.out.println("Invalid Name...!");
+            editContact();
+        }
+
+    }
+
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         addressBook.addContact();
+        addressBook.editContact();
     }
-
 
 }
